149511a42a91c7c12b4146f4ff36292af5fab867
joshuaubachi-ops <joshuaubachi@gmail.com>
2025-12-02 22:09:49 +0100

    Add files via upload

---
+++ kobe.py
@@ -0,0 +1,22 @@
+print("..........welcome to formula chooser..........")
+print("1. formula A\n2. formula B\n3. formula C\n4. formula D")
+choice = input("choose a formula from (1-4)")
+while True:
+    if choice == "1":
+        print("your now using formula A")
+        break
+    elif choice == "2":
+        print("your now using formula B")
+        break
+    elif choice == "3":
+        print("your now using formula C")
+        break
+    elif choice == "4":
+        print("your now using formula D")
+        break
+    else:
+        print("invalid option")
+
+
+
+
